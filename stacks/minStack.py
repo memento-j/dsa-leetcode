a4f1901c3330e7393d3001a253d7e488f4941a46
jsales2 <jsales0204@gmail.com>
2026-09-03 23:26:46 -0400

    leetcode stack problems

---
+++ stacks/minStack.py
@@ -0,0 +1,35 @@
+#155. Min Stack
+
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+# Implement the MinStack class:
+
+# MinStack() initializes the stack object.
+# void push(int value) pushes the element value onto the stack.
+# void pop() removes the element on the top of the stack.
+# int top() gets the top element of the stack.
+# int getMin() retrieves the minimum element in the stack.
+# You must implement a solution with O(1) time complexity for each function.
+
+class MinStack:
+    def __init__(self):
+        return None     
+
+    def push(self, value: int) -> None:
+        return None
+
+    def pop(self) -> None:
+        return None
+
+    def top(self) -> int:
+        return None
+
+    def getMin(self) -> int:
+        return None
+    
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
