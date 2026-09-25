4b8a77dcda4bd3bbbdbba87ff6653afce43c65f3
jsales2 <jsales0204@gmail.com>
2026-09-25 17:46:55 -0400

    test cases for goodnodes

--- trees/count_good_nodes.py
+++ trees/count_good_nodes.py
@@ -11,9 +11,104 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def goodNodes(self, root: TreeNode) -> int:
+def goodNodes(root: TreeNode) -> int:
     return
 
 
+
+####
+#### TEST CASES
+####
 if __name__ == "__main__":
-    print("hi")
+    # Example 1
+    #       3
+    #      / \
+    #     1   4
+    #    /   / \
+    #   3   1   5
+    #
+    # Expected: 4
+
+    root1 = TreeNode(
+        3,
+        TreeNode(
+            1,
+            TreeNode(3)
+        ),
+        TreeNode(
+            4,
+            TreeNode(1),
+            TreeNode(5)
+        )
+    )
+
+    print(goodNodes(root1))  # 4
+
+
+    # Example 2
+    #       3
+    #      /
+    #     3
+    #    /
+    #   4
+    #  /
+    # 2
+    #
+    # Good nodes: 3,3,4
+    # Expected: 3
+
+    root2 = TreeNode(
+        3,
+        TreeNode(
+            3,
+            TreeNode(
+                4,
+                TreeNode(2)
+            )
+        )
+    )
+
+    print(goodNodes(root2))  # 3
+
+
+    # Example 3
+    #       1
+    #      / \
+    #     2   3
+    #
+    # Every node is good
+    # Expected: 3
+
+    root3 = TreeNode(
+        1,
+        TreeNode(2),
+        TreeNode(3)
+    )
+
+    print(goodNodes(root3))  # 3
+
+
+    # Example 4
+    #       5
+    #      / \
+    #     4   3
+    #    /     \
+    #   2       1
+    #
+    # Only root is good
+    # Expected: 1
+
+    root4 = TreeNode(
+        5,
+        TreeNode(
+            4,
+            TreeNode(2)
+        ),
+        TreeNode(
+            3,
+            None,
+            TreeNode(1)
+        )
+    )
+
+    print(goodNodes(root4))  # 1
